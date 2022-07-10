ef8ee733cc2b3e39ceb0df88aa1515cb7c5ddb8b
Bryce <brycemadelo07@gmail.com>
2022-07-10 10:16:08 +0800

    Added Json file

--- MyPDF-Resume-Creator/Form1.Designer.cs
+++ MyPDF-Resume-Creator/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MyPDF_Resume_Creator
 {
-    partial class Form1
+    partial class PDFConverter
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.lablTitle = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGen
+            // 
+            this.btnGen.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnGen.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnGen.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnGen.Location = new System.Drawing.Point(428, 273);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(105, 31);
+            this.btnGen.TabIndex = 5;
+            this.btnGen.Text = "Generate PDF";
+            this.btnGen.UseVisualStyleBackColor = false;
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnChoose.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnChoose.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnChoose.Location = new System.Drawing.Point(57, 273);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(75, 31);
+            this.btnChoose.TabIndex = 4;
+            this.btnChoose.Text = "Choose";
+            this.btnChoose.UseVisualStyleBackColor = false;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(57, 76);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(476, 171);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // lablTitle
+            // 
+            this.lablTitle.AutoSize = true;
+            this.lablTitle.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.lablTitle.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lablTitle.Location = new System.Drawing.Point(55, 31);
+            this.lablTitle.Name = "lablTitle";
+            this.lablTitle.Size = new System.Drawing.Size(484, 32);
+            this.lablTitle.TabIndex = 6;
+            this.lablTitle.Text = "CONVERT YOUR JSON RESUME TO PDF";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.MediumSpringGreen;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.Color.CornflowerBlue;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 15);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(564, 298);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // PDFConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(588, 321);
+            this.Controls.Add(this.lablTitle);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.btnChoose);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "PDFConverter";
+            this.Text = "PDF CONVERTER";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button btnGen;
+        private Button btnChoose;
+        private RichTextBox richTextBox1;
+        private Label lablTitle;
+        private DataGridView dataGridView1;
     }
 }
--- MyPDF-Resume-Creator/Form1.cs
+++ MyPDF-Resume-Creator/Form1.cs
@@ -1,10 +1,25 @@
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.draw;
 namespace MyPDF_Resume_Creator
 {
-    public partial class Form1 : Form
+    public partial class PDFConverter : Form
     {
-        public Form1()
+        
+        public PDFConverter()
         {
             InitializeComponent();
+        }
+
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
